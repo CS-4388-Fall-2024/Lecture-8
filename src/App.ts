28cdfb80028426485e6ddd9651d006e54513d031
Isayas Adhanom <isayasadhanom@gmail.com>
2024-09-25 17:00:26 -0500

    post-lecture 9 commit

--- src/App.ts
+++ src/App.ts
@@ -11,6 +11,10 @@ export class App extends gfx.GfxApp
     private ground: gfx.Mesh3;
     private skybox: gfx.Mesh3;
     private sphere: gfx.Mesh3;
+
+    private sphereVelocity: gfx.Vector3;
+
+    private sphereRadius: number;
     // --- Create the App class ----
     constructor()
     {
@@ -20,6 +24,9 @@ export class App extends gfx.GfxApp
         this.ground = gfx.Geometry3Factory.createBox(50,1,50);
         this.skybox = gfx.Geometry3Factory.createBox(100,100,100);
         this.sphere = gfx.Geometry3Factory.createSphere();
+
+        this.sphereVelocity = new gfx.Vector3();
+        this.sphereRadius = 1;
     }
 
 
@@ -32,8 +39,10 @@ export class App extends gfx.GfxApp
         this.ground.position.set(0,-0.5,0);
         this.ground.material.setColor(new gfx.Color(83/255, 209/255, 110/255));
 
+        //create ambient light to add uniform and constant lighting to everything in the scene
         const ambientLight = new gfx.AmbientLight(new gfx.Color(0.4,0.4,0.4));
 
+        //create directional light that is infinitely far away like sunlight
         const directionalLight = new gfx.DirectionalLight(new gfx.Color(0.6,0.6,0.6));
         directionalLight.position.set(1,2,1);
 
@@ -42,7 +51,8 @@ export class App extends gfx.GfxApp
         this.skybox.material.setColor(new gfx.Color(0.698,1,1));
         
 
-        this.sphere.position.set(0,2,-6);
+        this.sphere.position.set(0,1,-10);
+        this.sphereVelocity.set(0,7,0);
         
 
         this.scene.add(this.ground);
@@ -58,7 +68,39 @@ export class App extends gfx.GfxApp
     // --- Update is called once each frame by the main graphics loop ---
     update(deltaTime: number): void 
     {
-        
+        //set accelaration constant
+        const spehereAccelaration = new gfx.Vector3(0,-3,0);   // accelarion in m/sec/sec
+
+        //v_new = v_old + a*dt
+
+        const a_dt = gfx.Vector3.multiplyScalar(spehereAccelaration, deltaTime);
+
+        this.sphereVelocity.add(a_dt);
+
+        //p_new = p_old + v_new*dt
+
+        const v_dt = gfx.Vector3.multiplyScalar(this.sphereVelocity, deltaTime);
+
+        this.sphere.position.add(v_dt);
+
+        if (this.sphere.position.y - this.sphereRadius < 0){
+            
+            //collision position correction
+            this.sphere.position.y = this.sphereRadius;
+
+            //reflected velocity
+            this.sphereVelocity.y *= -1.0;
+
+            //slow down the bounce to take friction into consideration
+            const frictionSlowDown = 0.85;
+            this.sphereVelocity.multiplyScalar(frictionSlowDown);
+
+            //debug sphere's position to determine if and where collision was detected
+            console.log("collision detected: ", this.sphere.position.y);
+
+            //Pause simulation to check detected collision.
+            //this.pause()
+        }
 
     }
 }
